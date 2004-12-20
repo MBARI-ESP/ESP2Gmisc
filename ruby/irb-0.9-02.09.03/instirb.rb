e3d34d2c9dce77df1a36319a0ef232d1e19be73e
Brent Roman <brent@mbari.org>
2004-12-20 05:21:08 +0000

    use rubylibdir config item don't copy CVS directories

--- ruby/irb-0.9-02.09.03/instirb.rb
+++ ruby/irb-0.9-02.09.03/instirb.rb
@@ -5,9 +5,8 @@ include Config
 
 destdir = ARGV[0] || ''
 
-version = "/"+CONFIG["MAJOR"]+"."+CONFIG["MINOR"]
 bindir = destdir+CONFIG["bindir"]
-rubylibdir = destdir+CONFIG["prefix"]+"/lib/ruby"+version
+rubylibdir = destdir+CONFIG["rubylibdir"]
 
 $:.unshift CONFIG["srcdir"]+"/lib"
 
@@ -17,7 +16,7 @@ require "find"
 File.install "bin/irb.rb", "#{bindir}/irb", 0755, true
 
 Find.find("lib") do |f|
-  next if File.directory? f
+  next if f.include?('/CVS/') or File.directory?(f)
   dir = rubylibdir+"/"+File.dirname(f[4..-1])
   File.makedirs dir, true unless File.directory? dir
   File.install f, dir, 0644, true
