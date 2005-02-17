b91ca171311a01f00e60384c6a8a7df5d37f2e7b
Brent Roman <brent@mbari.org>
2005-02-17 07:25:40 +0000

    Use File::Separator

--- ruby/inst-mbari.rb
+++ ruby/inst-mbari.rb
@@ -8,20 +8,22 @@ include Config
 
 destdir = ARGV[0] || ''
 
-bindir = destdir+CONFIG["bindir"]
+sitebindir = destdir+CONFIG["sitearchdir"]
 sitelibdir = destdir+CONFIG["sitelibdir"]
 
-$:.unshift CONFIG["srcdir"]+"/lib"
+puts sitebindir,sitelibdir
+
+$:.unshift CONFIG["srcdir"]+File::Separator+"lib"
 
 require "ftools"
 
 for f in BIN_IRB_TOOLS
-  File.install f, "#{bindir}/#{File.basename(f, '.rb')}", 0755, true
+  File.install f, sitebindir+File::Separator+File.basename(f, '.rb'), 0755, true
 end
 
 
 for f in LIB_IRB_TOOLS
-  File.install f, sitelibdir, 0644, true
+  File.install f, sitelibdir+File::Separator+File.basename(f), 0644, true
 end
 
 
