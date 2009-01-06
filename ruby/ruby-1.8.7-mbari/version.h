e515b3fba2f5e9743a164bff600d6862648ef85f
Brent Roman <brent@mbari.org>
2009-01-06 01:11:57 +0000

    include STACK_WIPE_SITES options in MBARI release string

--- ruby/ruby-1.8.7-mbari/version.h
+++ ruby/ruby-1.8.7-mbari/version.h
@@ -1,15 +1,15 @@
 #define RUBY_VERSION "1.8.7"
-#define RUBY_RELEASE_DATE "2008-12-21"
+#define RUBY_RELEASE_DATE "2009-1-5"
 #define RUBY_VERSION_CODE 187
-#define RUBY_RELEASE_CODE 20081221
+#define RUBY_RELEASE_CODE 20090105
 #define RUBY_PATCHLEVEL 72
 
 #define RUBY_VERSION_MAJOR 1
 #define RUBY_VERSION_MINOR 8
 #define RUBY_VERSION_TEENY 7
-#define RUBY_RELEASE_YEAR 2008
-#define RUBY_RELEASE_MONTH 12
-#define RUBY_RELEASE_DAY 21
+#define RUBY_RELEASE_YEAR 2009
+#define RUBY_RELEASE_MONTH 1
+#define RUBY_RELEASE_DAY 5
 
 #ifdef RUBY_EXTERN
 RUBY_EXTERN const char ruby_version[];
@@ -25,7 +25,12 @@ RUBY_EXTERN const char *ruby_copyright;
 #define RUBY_BIRTH_MONTH 2
 #define RUBY_BIRTH_DAY 24
 
-#define RUBY_RELEASE_STR "MBARI 6 on patchlevel"
+#include "rubysig.h"
+
+#define string_arg(s) #s
+#define MBARI_RELEASE(wipe_sites) "MBARI 7/" string_arg(wipe_sites)
+
+#define RUBY_RELEASE_STR MBARI_RELEASE(STACK_WIPE_SITES) " on patchlevel"
 #define RUBY_RELEASE_NUM RUBY_PATCHLEVEL
 
 
